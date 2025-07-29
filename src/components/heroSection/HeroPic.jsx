0e8708c1f7143e117beca4807fd6d6729cfc31de
Nayan <nayan.hajare28@gmail.com>
2025-07-30 01:03:33 +0530

    protfolio

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="public/images/Nayan_Pic.png"
+        src="/images/Nayan_Pic.png"
         alt="Nayan Hajare"
         className="max-h-[450px] w-auto"
       />
